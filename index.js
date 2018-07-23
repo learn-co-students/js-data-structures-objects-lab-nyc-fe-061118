97005aa4ad4c32b78358f742c9c35293e59be4c8
fiolam <fiola.mulliqi@wework.com>
2018-07-23 23:57:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+let driver={}
+function updateDriverWithKeyAndValue(driver,key,value){
+
+return {...driver, [key]:value}
+
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+driver[key] = value
+return driver
+}
+function deleteFromDriverByKey(driver,key){
+let copyDriver={...driver}
+  delete copyDriver[key]
+  return copyDriver
+} 
+function destructivelyDeleteFromDriverByKey(driver,key){
+ delete driver[key]
+  return driver
+}
